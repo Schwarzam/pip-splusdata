e769b7a4d879ccbb2209781b6c6cf3cc4c796f11
schwarzam <oliveira@oliveiras-MacBook-Pro.local>
2020-10-18 21:02:35 -0300

    gitignore

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setuptools.setup(
      packages = setuptools.find_packages(),
      author="Gustavo Schwarz",
      author_email="gustavo.b.schwarz@gmail.com",
-     description="Get data a lot of data within minutes",
-     url="https://github.com/schwarzam/pip-splusdata",
+     description="__",
+     url="https://github.com/schwarzam/splusdata",
      install_requires = ['pandas', 'astropy', 'sqlalchemy', 'psycopg2-binary', 'numpy', 'matplotlib', 'colour', 'pillow', 'requests', 'scipy'],
      classifiers=[
          "Programming Language :: Python :: 3",
